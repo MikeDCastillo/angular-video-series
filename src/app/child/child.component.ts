29639394c0af8d066c91cce548f21465375eff75
Mike-Castillo_ghub <Mike.Castillo@blueyonder.com>
2022-08-05 12:43:01 -0500

    @Output Child to Parent

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -6,12 +6,17 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./child.component.css']
 })
 export class ChildComponent implements OnInit {
-  @Input() childMessage!: string;
+  @Output() messageEvent = new EventEmitter<string>();
+  
   // @Output decorator is for when trying to send data from the child to the parent 
     // for when buttons are clicked, form entries, other user events. needs EventEmitter
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  sendMessage() {
+    this.messageEvent.emit('Hello from child'); // the value to emit
   }
 
 }
