11d9d9a3abb9aac6a221a670393d55c3ef71acc7
Mike-Castillo_ghub <Mike.Castillo@blueyonder.com>
2022-08-05 10:22:32 -0500

    Creates timer via Lifecycle Hooks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { // To add any lifecycle hook, just add NG to the interface
-  title = 'angular-video-series';
+export class AppComponent implements OnInit, OnDestroy { // To add any lifecycle hook, just add NG to the interface
+ 
+  intervalSub : any;
+
+  ngOnInit() { // Console logs every interval
+    this.intervalSub = setInterval(() => {
+      console.log('Hello from ngOnInit')
+    }, 1000);
+  }
+ 
+  ngOnDestroy() {
+    if (this.intervalSub) {
+      clearInterval(this.intervalSub);
+    }
+  }
+  
 }
